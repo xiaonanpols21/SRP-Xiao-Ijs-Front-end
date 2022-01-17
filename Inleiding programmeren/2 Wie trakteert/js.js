53f186c44e9fad6328995a36ae501c29c3bf2399
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2022-01-17 15:43:13 +0100

    Opdracht 2

--- Inleiding programmeren/2 Wie trakteert/js.js
+++ Inleiding programmeren/2 Wie trakteert/js.js
@@ -1,1 +1,15 @@
 console.log("hoi");
+
+var namen = ["Xiao Nan", "Chu Xia", "Jin", "Namjoon", "Hobi", "Taehyung"]
+
+var pEl = document.querySelector(".naam");
+
+var randomGetal = Math.random()*6;
+
+pEl.textContent = namen[2];
+
+randomGetal = Math.floor(randomGetal);
+
+console.log(randomGetal);
+
+pEl.textContent = namen[randomGetal];
