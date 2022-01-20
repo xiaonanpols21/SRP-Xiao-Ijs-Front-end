cba138d13a2e6d4422e45d1573317dec1dc269db
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2022-01-20 19:57:53 +0100

    Ducket 1

--- Duckett/1 Variable - Arrays/Nummers opslaan/js.js
+++ Duckett/1 Variable - Arrays/Nummers opslaan/js.js
@@ -1,17 +1,40 @@
 console.log("hoi");
 
-var bodyElement = document.querySelector("body");
+// S-1
+var price;
+var quantity;
+var total;
 
-console.log(bodyElement);
+price = 5;
+quantity = 14;
+total = price * quantity;
 
-// bodyElement.classList.add("glitch");
+var el = document.getElementById("cost");
+el.textContent = '$' + total;
+// Totaalprijs wordt opgeteld en neergezet in de HTML bij ID cost
 
-// Bonus
-var buttonEl = document.querySelector("button");
+// S-2
+var username;
+var message;
+username = "Molly";
+message = "See our upcomming range";
+
+var elName = document.getElementById("name");
+elName.textContent = username;
+var elNote = document.getElementById("note");
+elNote.textContent = message;
+// Tekst wordt veranderd en neergezet in de HTML bij de ID's
+
+// S-3
+var title;
+var message3;
+title = "Molly's Special offers";
+message3 = "<a href=\"sale.html\">25% off</a>";
+
+var elTitle = document.getElementById("title-3");
+elTitle.innerHTML = title;
+var elNote3 = document.getElementById("note-3");
+elNote3.innerHTML = message3;
+// Deze opdracht is om een quote te gebruiken in een string.
 
 
-function click() {
-    bodyElement.classList.add("glitch");
-}
-
-buttonEl.addEventListener("click", click);
