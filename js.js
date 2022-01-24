5f9754f8e888f88a1f5f5e251fe016af18e54409
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2022-01-24 21:28:10 +0100

    js

---
+++ js.js
@@ -0,0 +1,25 @@
+// Footer knop links weg
+var btnStap1 = document.querySelector("#btnStap1");
+var footer1Btn = document.querySelector("#footer1Btn");
+var bar = document.querySelector(".bar-clmn");
+
+// Klein hoorntje selected
+// var stap1KeuzeBtns = document.querySelector("figure");
+
+
+
+
+
+
+// Footer knop links weg
+btnStap1.classList.add("none");
+footer1Btn.classList.add("footer1Btn");
+bar.classList.add("marginR30");
+
+// Klein hoorntje selected
+// function stap1Select() {
+//     stap1KeuzeBtns.classList.add("stap1Border");
+// }
+
+// stap1KeuzeBtns.addEventListener("click", stap1Select);
+
